8899646b2ae2be13af18df1df7e27c4ee4f245d3
Abraham Luna Cazares <abraham11999@hotmail.com>
2021-09-19 22:34:15 -0600

    Formulario creado

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { SplashScreen } from '@capacitor/splash-screen';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  public form: FormGroup
+  public items: any = []
+
+  constructor(
+    fb: FormBuilder
+  ) {
+    this.form = fb.group({
+      tipo: [null, Validators.required],
+      folio: [null, Validators.required],
+      asunto: [null, Validators.required],
+      administrador: [null, Validators.required],
+      responsable: [null, Validators.required],
+      telefonoResponsable: [null, Validators.required],
+      lugar: [null, Validators.required]
+    })
+   }
+
+  ngOnInit() {
+    console.log("Mostrando Splash Screen");
+    
+    SplashScreen.show({
+      showDuration: 2000,
+      autoHide: true
+    });
+  }
 
 }
---
+++ src/app/modal-item/modal-item.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-item',
+  templateUrl: './modal-item.component.html',
+  styleUrls: ['./modal-item.component.scss'],
+})
+export class ModalItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}

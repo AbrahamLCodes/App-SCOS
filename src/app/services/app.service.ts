bc61b439da8d2700745f293ade9ddbb1fea4cf1d
Abraham Luna Cazares <abraham11999@hotmail.com>
2021-09-28 00:44:27 -0600

    Contenido del PDF y marca de agua

--- src/app/services/app.service.ts
+++ src/app/services/app.service.ts
@@ -85,6 +85,54 @@ export class AppService {
     messageAlert.present()
   }
 
+  public construirRenglones() {
+    const items = this.getReporteObject()
+    const rows = []
+    items.forEach(item => {
+      const imagenesArray = []
+      item.base64Images.map(imagen64 => {
+        imagenesArray.push([{ image: imagen64, width: 120, margin: [40, 10, 10, 0] }])
+      })
+      rows.push(
+        [
+          {
+            table: {
+              body: [
+                [{ text: item.asunto, alignment: "left", bold: true }],
+                [{ text: item.descripcion, alignment: "justify", bold: false }]
+              ]
+            },
+            margin: [0, 20, 0, 0],
+            layout: "noBorders"
+          },
+          {
+            table: {
+              body: imagenesArray
+            },
+            margin: [0, 20, 0, 0],
+            layout: "noBorders"
+          }
+        ]
+      )
+      rows.push(
+        [
+          { canvas: [{ type: 'line', x1: 20, y1: 0, x2: 520, y2: 0, lineWidth: 1 }] },
+          {}
+        ]
+      )
+    })
+
+    const contentTable = {
+      table: {
+        body: rows,
+        widths: [300, 200]
+      },
+      layout: "noBorders"
+    }
+
+    return contentTable
+  }
+
   public async generarPDF(datos: any) {
     const tipo = datos.tipo
     const folio = datos.folio
@@ -93,26 +141,26 @@ export class AppService {
     const responsable = datos.responsable
     const telefono = datos.telefono
     const lugar = datos.lugar
-    const items = this.getReporteObject()
 
     const logoSCOS = await this.getBase64ImageFromURL("../../assets/scos_logo.png")
+    const logoSCOSRotated = await this.getBase64ImageFromURL("../../assets/scos_rotated.png")
 
     const docDefinition = {
       pageSize: "A4",
-      pageMargins: [20, 80, 20, 70],
-      /*background: (currentPage, pageSize) => {
-        return { image: this.writeRotatedText("asdoahdsoashda"), width: 200, absolutePosition: { x: 50, y: 50 }, opacity: 0.1 }
-      },*/
+      pageMargins: [20, 130, 20, 70],
+      background: function (currentPage, pageSize) {
+        return { image: logoSCOSRotated, width: 300, opacity: 0.1, absolutePosition: { x: 150, y: 370 } }
+      },
       header: {
         margin: 10,
         columns: [
-          { image: logoSCOS, width: 120, margin: [35, 32, 10, 0] },
+          { image: logoSCOS, width: 120, margin: [35, 32, 10, 20] },
           {
             table: {
               widths: ["*"],
               body: [
                 [{ text: "Seguridad electrónica", alignment: "right", bold: true }],
-                [{ text: "Folio: 3", alignment: "right", bold: true, margin: [0, 0, 30, 0] }]
+                [{ text: "Folio: " + folio, alignment: "right", bold: true, margin: [0, 0, 30, 0] }]
               ]
             },
             margin: [0, 32, 25, 0],
@@ -127,16 +175,17 @@ export class AppService {
           margin: [0, 100, 0, 0],
           alignment: "right"
         },
-        { text: "Administrador del reporte", margin: [0, 0, 0, 20] },
-        { text: "Administrador del fracc. CD Juarez" },
-        { text: "Asnto: Asunto del reporte" },
+        { text: administrador, margin: [0, 0, 0, 20] },
+        { text: "Administrador de " + lugar },
+        { text: "Asnto: " + asunto },
         { text: "Presente.-" },
-        { text: "Por medio del presente informo los hallazgos encontrados en el CDJuarez" },
-        { text: "Atentamente", alignment: "center", margin: [0, 100, 0, 0] },
-        { canvas: [{ type: 'line', x1: 0, y1: 0, x2: 180, y2: 0, lineWidth: 1 }], margin: [0, 40, 0, 0], alignment: "center" },
-        { text: "Responsable del reporte", alignment: "center" },
-        { text: "Cel. telefono del responsable", alignment: "center" },
-
+        { text: "Por medio del presente informo los hallazgos encontrados en " + lugar },
+        { canvas: [{ type: 'line', x1: 40, y1: 0, x2: 560, y2: 0, dash: { length: 1, space: 5 }, lineWidth: 1 }], margin: [0, 40, 0, 0], alignment: "center" },
+        this.construirRenglones(),
+        { text: "Atentamente", absolutePosition: { y: 690 }, alignment: "center", margin: [0, 100, 0, 0] },
+        { canvas: [{ type: 'line', x1: 0, y1: 0, x2: 180, y2: 0, lineWidth: 1 }], absolutePosition: { y: 735 }, margin: [0, 40, 0, 0], alignment: "center" },
+        { text: "responsable", alignment: "center", absolutePosition: { y: 740 } },
+        { text: "65678898899", alignment: "center", absolutePosition: { y: 755 } },
       ],
       footer: {
         columns: [
@@ -159,9 +208,8 @@ export class AppService {
     pdfMake.createPdf(docDefinition).open()
   }
 
-  public writeRotatedText = text => {
-    let ctx,
-      canvas = document.createElement('canvas');
+  public writeRotatedImage = imagen => {
+    let ctx: CanvasRenderingContext2D, canvas = document.createElement('canvas');
     canvas.width = 50;
     canvas.height = 300;
     ctx = canvas.getContext('2d');
@@ -170,8 +218,17 @@ export class AppService {
     ctx.translate(50, 300);
     ctx.rotate(-0.5 * Math.PI);
     ctx.fillStyle = '#000';
-    ctx.fillText(text, 0, 0);
-    ctx.restore();
+
+    let image = new Image()
+    image.src = imagen
+    new Promise((resolve, reject) => {
+      image.onload = () => {
+        ctx.drawImage(image, 50, 50)
+        console.log("Dibujando imagen");
+      }
+    })
+
+    ctx.restore()
     return canvas.toDataURL();
   };
 
